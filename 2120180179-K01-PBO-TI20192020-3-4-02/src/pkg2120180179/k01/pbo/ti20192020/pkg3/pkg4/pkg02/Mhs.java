46b564a3dffac233cfad5d301b3dfe4c72c05e5c
khaniefbochil <khanief.islahudin556@gmail.com>
2019-10-24 14:18:32 +0700

    apload tugas kuis 01

---
+++ 2120180179-K01-PBO-TI20192020-3-4-02/src/pkg2120180179/k01/pbo/ti20192020/pkg3/pkg4/pkg02/Mhs.java
@@ -0,0 +1,13 @@
+package pkg2120180179.k01.pbo.ti20192020.pkg3.pkg4.pkg02;
+public class Mhs {
+    //atribut class Mhs yaitu nama,nim,alamat. dengan tipe data String dan bersifat public
+    public String nama ="";
+    public String nim ="";
+    public String alamat ="";
+    //deklarasi method dengan type method String
+    public void tampilBiodata(String nama, String nim, String alamat){
+        System.out.println("nama   ="+nama);
+        System.out.println("nim    ="+nim);
+        System.out.println("alamat ="+alamat);
+    }
+}
