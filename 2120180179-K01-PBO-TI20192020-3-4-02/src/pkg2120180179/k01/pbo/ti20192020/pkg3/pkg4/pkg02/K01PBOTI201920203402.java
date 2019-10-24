421f881cdf4fc12ba5c5706e070ed03ecf98c104
khaniefbochil <khanief.islahudin556@gmail.com>
2019-10-24 14:12:23 +0700

    aploat tugas kuis 01

---
+++ 2120180179-K01-PBO-TI20192020-3-4-02/src/pkg2120180179/k01/pbo/ti20192020/pkg3/pkg4/pkg02/K01PBOTI201920203402.java
@@ -0,0 +1,28 @@
+package pkg2120180179.k01.pbo.ti20192020.pkg3.pkg4.pkg02;
+public class K01PBOTI201920203402 {
+    public static void main(String[] args) {
+    //instansiasi dari kelas Mhs
+        //membuat objek teman1 dari kelas Mhs
+        Mhs teman=new Mhs();
+        //membuat objek teman1 dari kelas Mhs
+        Mhs teman1=new Mhs();
+        //membuat objek teman2 dari kelas Mhs
+        Mhs teman2=new Mhs();
+        //membuat objek teman3 dari kelas Mhs
+        Mhs teman3=new Mhs();
+        //membuat objek teman4 dari kelas Mhs
+        Mhs teman4=new Mhs();
+        
+        System.out.println("Biodata Mhs     :\n");//memunculkan tulisan "Biodata Mhs     :"
+        //diatas hasil dari akses method
+        
+    //akses method
+        //memasukan biodata 5 teman dan memanggil method
+        teman.tampilBiodata(" kresnawan satrio A."," 21201802717"," Bojonegoro");
+        teman1.tampilBiodata(" Muhamad Edy W."," 2120180182"," Tuban");
+        teman2.tampilBiodata(" Siti Gurotul B."," 2120180201"," Bojonegoro");
+        teman3.tampilBiodata(" Muhamad Kafiudin"," 2120180178"," Boureno");
+        teman4.tampilBiodata(" Nuruz Zilvia"," 2120180196"," Bojonegoro");
+    }
+    
+}
